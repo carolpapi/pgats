007be9c6196390f32d27ce78a6ffa52adddeae5d
Carol Papi <carolina.papi@jazztech.com.br>
2024-05-19 17:59:06 -0300

    testando commit

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -2,37 +2,33 @@ const calcularTotal = (ferramentas, comprar) => {
     if ( ferramentas.length === 0 || comprar.length === 0 ) {
         throw new Error("Ambas as listas precisam ter ao menos um item.")
     } else {
-        let encontrar = false;
+        let ferramentasListadas = [];
         let itensEncontrados = 0;
-        let ferramentasListadas = []; 
-
+        let encontrado = false; 
         for (let indexComprar = 0; indexComprar < comprar.length; indexComprar++) {
             for (let indexFerramentas = 0; indexFerramentas < ferramentas.length; indexFerramentas++) {
                 if (comprar[indexComprar] === ferramentas[indexFerramentas].nome) {
-                    encontrar = true;
+                    encontrado = true;
                     itensEncontrados += ferramentas[indexFerramentas].preco;
                     ferramentasListadas[ferramentasListadas.length] = comprar[indexComprar];
                 }
             }
         }
-        
-        if (!encontrar) {
+        if (!encontrado) {
             throw new Error("Nenhuma ferramenta desejada encontrada.");
         } else {
-            const valorFormatado = itensEncontrados.toFixed(2);
-
-            let ferramentasFormatadas = ""
-            for (let k = 0; k < ferramentasListadas.length; k++) {
-                ferramentasFormatadas += ferramentasListadas[k];
-                if (k < ferramentasListadas.length - 1) {
-                    ferramentasListadas += ", ";
+            const valor = itensEncontrados.toFixed(2);
+            let ferramentasDesejadas = ""
+            for (let index = 0; index < ferramentasListadas.length; index++) {
+                ferramentasDesejadas += ferramentasListadas[index];
+                if (index < ferramentasListadas.length - 1) {
+                    ferramentasDesejadas += ", ";
                 }
             }
-            return `O valor a pagar pelas ferramentas (${ferramentasListadas}) é R$ ${valorFormatado}`;
+            return `O valor a pagar pelas ferramentas (${ferramentasDesejadas}) é R$ ${valor}`;
         }
     }
 }
-
 module.exports = {
     calcularTotal
 }
